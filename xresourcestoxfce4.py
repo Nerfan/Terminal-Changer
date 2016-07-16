0aea3602dd3813cc8bf0a2ffabbb7d3d2045e0c1
jeremy <jerelefurge@gmail.com>
2016-07-15 23:27:38 -0400

    Renamed the file to convert .Xresources to xfce4-terminal That script also now has defaults for file locations

---
+++ xresourcestoxfce4.py
@@ -0,0 +1,87 @@
+"""
+Take an .Xresources file and apply the colorscheme to xfce4-terminal
+
+The file doesn't have to be the .Xresources file, just in the same format.
+Enter the filename as a command line argument if not .Xresources.
+The script then scans for colors in the file and applies them to the terminal.
+Designed because I wanted a reusable script to change
+.Xresources config files into ones usable with xfce4-terminal.
+"""
+
+import sys
+from os import getenv
+
+HOME = getenv("HOME")
+
+def get_new_colors(filename):
+    """
+    Take a file and "extract" all of the color codes from it.
+
+    Args:
+        filename (str): Path to file containing color codes
+
+    Returns:
+        str: any color codes found in the text separated by semicolons
+    """
+    colorList = [""]*16 
+    for line in open(filename):
+        for i in range(len(line)):
+            if line[i:i+5] == "color" and line[i+5].isdigit():
+                index = line[i+5]
+                if line[i+6] != ":":
+                    index = line[i+5:i+7]
+                index = int(index)
+                colorList[index] = line.split()[1]
+    new_colors = ""
+    for color in colorList:
+        new_colors += color + ";"
+    return new_colors[:-1]
+
+def set_colors(new_colors, terminalrc_location=HOME + "/.config/xfce4/terminal/terminalrc"):
+    """
+    Take a string of color codes and apply to terminalrc.
+
+    Can also take an argument for where the terminalrc is
+
+    Args:
+        new_colors (str): 16 color codes separated by semicolons (no spaces)
+        terminalrc_location (str): Path to the xfce4-terminal config file
+    """
+    verify = input("Have you saved the current color scheme? (y/n) ")
+    if verify != "y":
+        print("Go ahead and do that.")
+        exit(0)
+    terminalrc = open(terminalrc_location)
+    filebefore = ""
+    for line in terminalrc:
+        if line[0:13] == "ColorPalette=":
+            filebefore += line[0:13] + new_colors + "\n"
+        else:
+            filebefore += line
+    terminalrc.close()
+    terminalrc = open(terminalrc_location, "w")
+    terminalrc.truncate()
+    terminalrc.write(filebefore)
+    terminalrc.close()
+
+def main(sourcefile=HOME + "/.Xresources", terminalrc=HOME + "/.config/xfce4/terminal/terminalrc"):
+    """
+    Loop through a file and change the current settings in terminalrc
+
+    Args:
+        sourcefile (str): Name of file to open containing the source colors
+        terminalrc (str): Path to terminalrc file
+    """
+    new_colors = get_new_colors(args[1])
+    if (len(args) == 3):
+        set_colors(new_colors, args[2])
+    else:
+        set_colors(new_colors)
+
+if __name__=="__main__":
+    if len(sys.argv) > 3:
+        print("Usage: python3.5 colorsToXfce4Terminal.py [sourcefile] [terminalrc]")
+        exit(0)
+    elif len(sys.argv) < 2:
+        main()
+    main(sys.argv[1], sys.argv[2])
